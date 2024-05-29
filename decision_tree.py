f612196809151780be3c0efc53dd2108985b44ed
benjamingebhardt7434 <benjamin.gebhardt@otto.de>
2024-05-29 17:02:54 +0200

    First decision tree tries

--- decision_tree.py
+++ decision_tree.py
@@ -33,3 +33,43 @@ def entropy(inputs):
     
     return 1. - abs(counts[0]-counts[1]) / len(inputs)
 # %%
+
+intial_entropy = entropy(grey_hair)
+
+best_entropy = intial_entropy
+
+best_split = None
+
+for split_point in age:
+    left = age < int(split_point)
+    right = age > int(split_point)
+
+    left_entropy = entropy(left)
+    right_entropy = entropy(right)
+
+    current_entropy = left_entropy + right_entropy
+
+    if current_entropy < best_entropy:
+        best_entropy = current_entropy
+        best_split = split_point
+
+#print("Best split point is at:", best_split, "with a entropy of:", best_entropy)
+
+
+## PROBLEM: Current best entropy is at split position 0? Should be an age, not 0.
+
+
+#Chaos, Vermischung, Diversion, Entropy minimieren, Einheitlichkeit maximieren:
+
+# Entropy-Wert: soll hoch sein, wenn wir viele verschiedene Klassen vertreten, Extremwert bei 50/50
+# soll klein/minimal sein, wenn wir eine einzige Klasse haben (0 bei 100%)
+
+# Einheitlichkeit = abs(Anzahl NG - Anzahl G) / Anzahl Elemente
+
+# Entropy = 1 - Einheitlichkeit
+#     = 1 - (abs(Anzahl NG - Anzahl G) / Anzahl Elemente)
+
+
+
+
+# %%
